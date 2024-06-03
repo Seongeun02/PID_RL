cf0411009e164ade945844c28da354ff356748cf
Seongeun02 <lizsarah@snu.ac.kr>
2024-06-04 00:08:39 +0900

    commit

---
+++ ddpg/CSTRenv.py
@@ -0,0 +1,234 @@
+import numpy as np
+import time
+from matplotlib import pyplot as plt
+from scipy.integrate import odeint
+import gym
+from gym import spaces
+
+
+class VV:
+  """
+  Assume isothermal reactor for simplicity
+  """
+  def __init__(self):
+    self.Ca0 =  5.1        # Inlet feed concentration (mol/m^3)
+    #self.T =  114.2       # reactor temperature (degC)
+    self.T = 110
+
+    self.k10 = 1.287e12    # A->B Pre-exponential factor (1/hr)
+    self.k20 = 1.287e12    # B->C Pre-exponential factor (1/hr)
+    self.k30 = 9.043e9     # 2A->D Pre-exponential factor (1/hr)
+    self.E1 = 9758.3       # A->B Activation Energy (K)
+    self.E2 = 9758.3       # B->C Activation Energy (K)
+    self.E3 = 8560         # 2A->D Activation Energy (K)
+    self.VR = 10.0         # Reactor volume (l)
+
+    # steady state value of VV
+    self.Ca_ss = 2.14
+    self.Cb_ss = 1.09
+    self.F_ss = 141.9
+
+  def arrenhius(self, T):
+    self.k1 = self.k10*np.exp(-self.E1/(T+273.15))
+    self.k2 = self.k20*np.exp(-self.E2/(T+273.15))
+    self.k3 = self.k30*np.exp(-self.E3/(T+273.15))
+
+  def linearlized_dynamics(self, s, t):
+    Ca_dev, Cb_dev, u = s
+    self.arrenhius(self.T)
+
+    dCadt_lin = (-self.F_ss / self.VR - self.k1 - 2*self.k3*self.Ca_ss) * Ca_dev \
+          + (self.Ca0-self.Ca_ss) / self.VR * u
+    dCbdt_lin = (-self.F_ss / self.VR - self.k2) * Cb_dev \
+          + self.k1*Ca_dev + (-self.Cb_ss / self.VR) * u
+    dudt = 0
+
+    return [dCadt_lin, dCbdt_lin, dudt]
+
+
+class PID:
+    """PID Controller
+    """
+
+    def __init__(self, P=0.2, I=0.0, D=0.0, current_time=None):
+
+        self.Kp = P
+        self.Ki = I
+        self.Kd = D
+
+        self.sample_time = 0.00
+        self.current_time = current_time if current_time is not None else time.time()
+        self.last_time = self.current_time
+
+        self.clear()
+
+    def clear(self):
+        """Clears PID computations and coefficients"""
+        self.SetPoint = 0.0
+
+        self.PTerm = 0.0
+        self.ITerm = 0.0
+        self.DTerm = 0.0
+        self.last_error = 0.0
+
+        # Windup Guard
+        self.int_error = 0.0
+        self.windup_guard = 20.0
+
+        self.output = 0.0
+
+    def update(self, feedback_value, current_time=None):
+        """Calculates PID value for given reference feedback
+
+        .. math::
+            u(t) = K_p e(t) + K_i \int_{0}^{t} e(t)dt + K_d {de}/{dt}
+
+        """
+        error = self.SetPoint - feedback_value
+
+        self.current_time = current_time if current_time is not None else time.time()
+        delta_time = self.current_time - self.last_time
+        delta_error = error - self.last_error
+
+        if (delta_time >= self.sample_time):
+            self.PTerm = self.Kp * error
+            self.ITerm += error * delta_time
+
+            if (self.ITerm < -self.windup_guard):
+                self.ITerm = -self.windup_guard
+            elif (self.ITerm > self.windup_guard):
+                self.ITerm = self.windup_guard
+
+            self.DTerm = 0.0
+            if delta_time > 0:
+                self.DTerm = delta_error / delta_time
+
+            # Remember last time and last error for next calculation
+            self.last_time = self.current_time
+            self.last_error = error
+
+            self.output = self.PTerm + (self.Ki * self.ITerm) + (self.Kd * self.DTerm)
+
+    def setKp(self, proportional_gain):
+        self.Kp = proportional_gain
+
+    def setKi(self, integral_gain):
+        self.Ki = integral_gain
+
+    def setKd(self, derivative_gain):
+        self.Kd = derivative_gain
+
+    def setWindup(self, windup):
+        """Integral windup, also known as integrator windup or reset windup,
+        refers to the situation in a PID feedback controller where
+        a large change in setpoint occurs (say a positive change)
+        and the integral terms accumulates a significant error
+        during the rise (windup), thus overshooting and continuing
+        to increase as this accumulated error is unwound
+        (offset by errors in the other direction).
+        The specific problem is the excess overshooting.
+        """
+        self.windup_guard = windup
+
+    def setSampleTime(self, sample_time):
+        self.sample_time = sample_time
+        
+        
+        
+class CSTRenv(gym.Env):
+    def __init__(self):
+        super(CSTRenv, self).__init__()
+        
+        num_PID = 1
+
+        # State Space
+        #self.state_space = spaces.Box(low=np.array([0.0, 0.0, 0.0]), high=np.array([50.0, 50.0, 50.0]), dtype=np.float32)
+        self.state_space = spaces.Box(low=np.zeros(2*num_PID), high=50*np.ones(2*num_PID), dtype=np.float32)
+
+        # Action Space
+        #self.action_space = spaces.Box(low=np.array([-1.0, -1.0, -1.0]), high=np.array([1.0, 1.0, 1.0]), dtype=np.float32)
+        self.action_space = spaces.Box(low=-1*np.ones(2*num_PID), high=np.ones(2*num_PID), dtype=np.float32)
+
+        # initial condition
+        self.F_init = 140.0
+        self.Ca_init = 2.2291     # Concentration of A in CSTR (mol/l)
+        self.Cb_init = 1.0417     # Concentration of B in CSTR (mol/l)
+        
+        # initial state
+        self.Kp = 5
+        self.Ki = 5
+        #self.Kd = 0
+    
+        self.Ca_setpoint = 3.0      
+        self.sample_time = 1/2
+        
+        self.alpha = 1
+        self.beta = 1
+
+        # initialize
+        self.reset()
+
+    def step(self, action):
+        #assert self.action_space.contains(action)   
+        
+        # update state
+        PID_params = {"Kp" : self.Kp + self.alpha*action[0],
+                      "Ki" : self.Ki + self.alpha*action[1]}
+                      #"Kd" : self.Kd + self.alpha*action[2]}
+        #assert self.state_space.contains(np.array(PID_params.values()))
+        
+        # VV system
+        vv_system = VV()
+        
+        # PID control
+        pid = PID(current_time = 0.0)
+        pid.setSampleTime(self.sample_time)
+        pid.setKp(PID_params["Kp"])
+        pid.setKi(PID_params["Ki"])
+        #pid.setKd(PID_params["Kd"])
+        pid.SetPoint = self.Ca_setpoint - vv_system.Ca_ss
+        
+        Ca = [self.Ca_init]
+        Cb = [self.Cb_init]
+        F = [self.F_init] # 실제 우리가 관측하게 될 유량
+        input = [self.F_init-vv_system.F_ss] #PID가 뱉어주는 입력값
+        
+        current_time = 0.0
+        sample_num = 1
+
+        while(sample_num <= 60):
+            Ca_dev_init = Ca[-1] - vv_system.Ca_ss
+            Cb_dev_init = Cb[-1] - vv_system.Cb_ss
+
+            s_init = (Ca_dev_init, Cb_dev_init, input[-1]) # dynamics에 넣는건 deviation variables
+            t = np.linspace(current_time, current_time + self.sample_time, num=int(self.sample_time*60))
+            s = odeint(func=vv_system.linearlized_dynamics, y0=s_init, t=t) #sample time 만큼 ode 풀기
+            current_Ca_dev, current_Cb_dev = [s[-1][0], s[-1][1]]
+
+            current_time += self.sample_time
+            pid.update(current_Cb_dev, current_time) # PID도 deviation variable로
+            input.append(pid.output)
+
+            for i in range(int(self.sample_time*60)):
+                Ca.append(s.T[0][i]+vv_system.Ca_ss)
+                Cb.append(s.T[1][i]+vv_system.Cb_ss)
+                F.append(s.T[2][i]+vv_system.F_ss)
+
+            sample_num += 1
+
+        # calculate reward
+        t_conv = np.argmin(Ca[-1]==Ca)/sample_num
+        reward = -abs(Ca[-1] - self.Ca_setpoint) - self.beta*t_conv
+        done = False
+        if abs(reward) < 0.02*self.Ca_setpoint:
+            done = True  
+            
+        return np.array(list(PID_params.values())), reward, done, {}
+
+    def reset(self):
+        # reset env to initial state
+        self.Kp = 5
+        self.Ki = 5
+        #self.Kd = 0
+        
+        return np.array([self.Kp, self.Ki, self.Kd])
---
+++ ddpg_extension/ddpg_v2.py
@@ -0,0 +1,215 @@
+import gym
+import random
+import collections
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from matplotlib import pyplot as plt
+import yaml
+
+import CSTRenv_v2
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# read yaml file
+with open('config.yml', 'r') as file:
+    config = yaml.safe_load(file)
+
+# Hyperparameters
+lr_mu = config['hyperparameters']['lr_mu']
+lr_q = config['hyperparameters']['lr_q']
+gamma = config['hyperparameters']['gamma']
+batch_size = config['hyperparameters']['batch_size']
+buffer_limit = config['hyperparameters']['buffer_limit']
+tau = config['hyperparameters']['tau']
+
+# OU process parameters
+theta = config['noise']['theta']
+dt = config['noise']['dt']
+sigma = config['noise']['sigma']
+
+# Training parameters
+num_episodes = config['training']['num_episodes']
+print_interval = config['training']['print_interval']
+start_train_size = config['training']['start_train_size']
+train_iterations = config['training']['train_iterations']
+
+# environment parameters
+F_init = config['environment']['F_init']
+Ca_init = config['environment']['Ca_init']
+Cb_init = config['environment']['Cb_init']
+Ca_setpoint = config['environment']['Ca_setpoint']
+sample_time = config['environment']['sample_time']
+alpha = config['environment']['alpha']
+beta = config['environment']['beta']
+delta = config['environment']['delta']      # threshold to start training PI controller from pure P controller
+
+# Replay Buffer
+class ReplayBuffer():
+    def __init__(self):
+        self.buffer = collections.deque(maxlen=buffer_limit)
+
+    def put(self, transition):
+        self.buffer.append(transition)
+    
+    def sample(self, n):
+        mini_batch = random.sample(self.buffer, n)
+        s_lst, a_lst, r_lst, s_prime_lst, done_mask_lst = [], [], [], [], []
+
+        for transition in mini_batch:
+            s, a, r, s_prime, done = transition
+            s_lst.append(s)
+            a_lst.append(a)
+            r_lst.append([r])
+            s_prime_lst.append(s_prime)
+            done_mask = 0.0 if done else 1.0 
+            done_mask_lst.append([done_mask])
+        
+        return torch.tensor(s_lst, dtype=torch.float).to(DEVICE), \
+               torch.tensor(a_lst, dtype=torch.float).to(DEVICE), \
+               torch.tensor(r_lst, dtype=torch.float).to(DEVICE), \
+               torch.tensor(s_prime_lst, dtype=torch.float).to(DEVICE), \
+               torch.tensor(done_mask_lst, dtype=torch.float).to(DEVICE)
+    
+    def size(self):
+        return len(self.buffer)
+    
+
+# actor network
+class MuNet(nn.Module):
+    def __init__(self):
+        super(MuNet, self).__init__()
+        self.fc1 = nn.Linear(2, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc_mu = nn.Linear(64, 1)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        mu = torch.tanh(self.fc_mu(x))  # range [-1,1]
+        return mu
+
+# critic network
+class QNet(nn.Module):
+    def __init__(self):
+        super(QNet, self).__init__()
+        self.fc_s = nn.Linear(2, 64)
+        self.fc_a = nn.Linear(1,64)
+        self.fc_q = nn.Linear(128, 32)
+        self.fc_out = nn.Linear(32,1)
+
+    def forward(self, x, a):
+        h1 = F.relu(self.fc_s(x))
+        h2 = F.relu(self.fc_a(a))
+        cat = torch.cat([h1,h2], dim=1)
+        q = F.relu(self.fc_q(cat))
+        q = self.fc_out(q)
+        return q
+
+# OU process
+class OrnsteinUhlenbeckNoise:
+    def __init__(self, mu):
+        self.theta, self.dt, self.sigma = theta, dt, sigma
+        self.mu = mu
+        self.x_prev = np.zeros_like(self.mu)
+
+    def __call__(self):
+        x = self.x_prev + self.theta * (self.mu - self.x_prev) * self.dt + \
+                self.sigma * np.sqrt(self.dt) * np.random.normal(size=self.mu.shape)
+        self.x_prev = x
+        return x
+    
+# train
+def train(mu, mu_target, q, q_target, memory, q_optimizer, mu_optimizer):
+    s,a,r,s_prime,done_mask  = memory.sample(batch_size)
+    
+    target = r + gamma * q_target(s_prime, mu_target(s_prime)) * done_mask
+    q_loss = F.smooth_l1_loss(q(s,a), target.detach())
+    q_optimizer.zero_grad()
+    q_loss.backward()
+    q_optimizer.step()
+    
+    mu_loss = -q(s,mu(s)).mean() # That's all for the policy loss.
+    mu_optimizer.zero_grad()
+    mu_loss.backward()
+    mu_optimizer.step()
+    
+def soft_update(net, net_target):
+    for param_target, param in zip(net_target.parameters(), net.parameters()):
+        param_target.data.copy_(param_target.data * (1.0 - tau) + param.data * tau)
+        
+if __name__ == '__main__':
+    DEVICE = torch.device('cuda') if torch.cuda.is_available else torch.device('cpu')
+    print("Using PyTorch version: {}, Device: {}".format(torch.__version__, DEVICE))
+    
+    env = CSTRenv_v2.CSTRenv(
+                            F_init = F_init,
+                            Ca_init = Ca_init,
+                            Cb_init = Cb_init,
+                            Ca_setpoint = Ca_setpoint,
+                            sample_time = sample_time,
+                            alpha = alpha,
+                            beta = beta,
+                            delta = delta)
+    memory = ReplayBuffer()
+
+    ## critic 
+    q, q_target = QNet().to(DEVICE), QNet().to(DEVICE)
+    q_target.load_state_dict(q.state_dict())
+    
+    ## actor for Kp
+    mu_p, mu_target_p = MuNet().to(DEVICE), MuNet().to(DEVICE)
+    mu_target_p.load_state_dict(mu_p.state_dict())
+    
+    ## actor for Ki
+    mu_i, mu_target_i = MuNet().to(DEVICE), MuNet().to(DEVICE)
+    mu_target_i.load_state_dict(mu_i.state_dict())
+
+    score = []
+    print_interval = 20
+
+    mu_p_optimizer = optim.Adam(mu_p.parameters(), lr=lr_mu)
+    mu_i_optimizer = optim.Adam(mu_i.parameters(), lr=lr_mu)
+    q_optimizer  = optim.Adam(q.parameters(), lr=lr_q)
+    ou_noise = OrnsteinUhlenbeckNoise(mu=np.zeros(1))
+
+    for n_epi in range(num_episodes):
+        s = env.reset()  # initial state
+        done = False
+
+        count = 0
+        update_phase = "Kp"
+        while count < 200 and not done:
+            s_tensor = torch.from_numpy(np.array(s)).float().to(DEVICE)
+            if update_phase == "Kp":
+                a = mu_p(s_tensor).cpu().detach().numpy()
+            elif update_phase == "Ki":
+                a = mu_i(s_tensor).cpu().detach().numpy()
+            a = [a[i] + ou_noise()[0] for i in range(len(a))]
+            
+            s_prime, r, done, update_phase = env.step(a)
+            memory.put((s,a,r/100.0,s_prime,done))
+            score.append(r)
+            s = s_prime
+            count += 1
+                
+            if memory.size() > start_train_size:
+                if update_phase == 'Kp':
+                    for i in range(train_iterations):
+                        train(mu_p, mu_target_p, q, q_target, memory, q_optimizer, mu_p_optimizer)
+                        soft_update(mu_p, mu_target_p)
+                        soft_update(q,  q_target)
+                elif update_phase == 'Ki':
+                    for i in range(train_iterations):
+                        train(mu_i, mu_target_i, q, q_target, memory, q_optimizer, mu_i_optimizer)
+                        soft_update(mu_i, mu_target_i)
+                        soft_update(q,  q_target)                    
+                
+        
+        if n_epi%print_interval==0 and n_epi!=0:
+            print("# of episode :{}, avg score : {:.1f}".format(n_epi, sum(score[count-print_interval:count])/print_interval))
+
+    env.close()
